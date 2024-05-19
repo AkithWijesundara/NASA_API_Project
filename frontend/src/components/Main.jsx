c2cf50bed547f8f1e27a4085b13f513e91aa0342
akithwijesundara <akithwijesundara@gmail.com>
2024-05-19 17:37:50 +0530

    akith comit on new branch

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -7,7 +7,18 @@ export default function Main({
 }) {
   return (
     <div className='image-container'>
+        {image.media_type === 'image'?(
         <img src={image.hdurl} alt={image.title} className="bgImage" onClick={() => onImageClick(image)} />
+      ):image.media_type === 'video'?(
+        <iframe
+          src={image.url}
+          title={image.title}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      ):(
+        <p>Resourse Type Not identified</p>
+      )}
     </div>
   )
 }
